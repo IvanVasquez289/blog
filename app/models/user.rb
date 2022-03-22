ff073d452d80aba315f7703987f88bbdb7c24552
Ivan Vasquez <ivanjv1234@gmail.com>
2022-03-22 00:12:10 -0600

    feat: before validation to downcase in email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_validation {self.email = email.downcase}
   has_many :articles
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
