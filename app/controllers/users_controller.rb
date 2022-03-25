7341cd16d47b7f34884d3b545750fdc2841f9f4c
Ivan Vasquez <ivanjv1234@gmail.com>
2022-03-25 15:05:43 -0600

    style: add gravatar in show article and add redirects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Bienvenido a Alpha Blog #{@user.username}, te has registrado correctamente"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'new'
     end
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Tu informacion ha sido actualizada"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
