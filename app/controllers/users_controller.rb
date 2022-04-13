10eb1f4edd66d2f73fee01b883d18a6931ef82d3
Ivan Vasquez <ivanjv1234@gmail.com>
2022-04-12 19:59:58 -0500

    add helper methods, DRY, modified the navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+
+
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find(params[:id])
+   
     @articles = @user.articles.paginate(page: params[:page], per_page: 4)
   end
   
@@ -16,6 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Bienvenido a Alpha Blog #{@user.username}, te has registrado correctamente"
       redirect_to @user
     else
@@ -24,11 +28,11 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    
   end
   
   def update
-    @user = User.find(params[:id])
+    
     if @user.update(user_params)
       flash[:notice] = "Tu informacion ha sido actualizada"
       redirect_to @user
@@ -39,7 +43,10 @@ class UsersController < ApplicationController
   
 
   private
-
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,14 @@ module ApplicationHelper
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{tamano}"
     image_tag(gravatar_url, alt: user.username, class:"rounded mx-auto d-block shadow")
   end
+
+
+  def current_user
+    @current_userr ||= User.find(session[:user_id]) if session[:user_id]  
+  end
+
+  def logged_in?
+    !!current_user
+  end
+  
 end
