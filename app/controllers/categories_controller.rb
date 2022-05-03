884ec48ba1fa614a3d9e0ba4b5d1219304ba35ce
Ivan Vasquez <ivanjv1234@gmail.com>
2022-05-02 19:57:13 -0500

    test: Complete categories create action and functional test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,17 @@
 class CategoriesController < ApplicationController
   def new
-    
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = 'La categoria fue agregada exitosamente'
+      redirect_to @category
+    else
+      render 'new'
+    end
   end
 
   def index
@@ -9,6 +20,12 @@ class CategoriesController < ApplicationController
 
   def show
 
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
   
 end
